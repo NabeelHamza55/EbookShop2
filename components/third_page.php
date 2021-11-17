cdd9d6d46886099a03e5f89fd8da3c66bea0693d
HammaadKhan <nogain447khan@gmail.com>
2021-11-17 13:46:41 +0500

    third page
    not yet done

---
+++ components/third_page.php
@@ -0,0 +1,77 @@
+<div class="third-page">
+<div class="container ">
+    <h1>Bestselling Books</h1>
+<div class="row  p-3 ">
+  <div class="col-sm border py-5 px-4"> 
+      <img src="./images/2560.jpg"  style="width: 100%;" height="200rem" alt="">
+
+      <p>Think Like a Monk: Train Your Mind Like a Peace & Purposes</p>
+
+  </div>
+  <div class="col-sm border py-5 px-4">
+  <img src="./images/2560.jpg" style="width: 100%;"  height="200rem" alt="">
+
+<p>Think Like a Monk: Train Your Mind Like a Peace & Purposes</p>
+  </div>
+  <div class="col-sm border py-5 px-4 ">
+  <img src="./images/2560.jpg" style="width: 100%;" height="200rem" alt="">
+
+<p>Think Like a Monk: Train Your Mind Like a Peace & Purposes</p>
+  </div>
+  <div class="col-sm border py-5 px-4">
+  <img src="./images/2560.jpg" style="width: 100%;"  height="200rem" alt="">
+
+<p>Think Like a Monk: Train Your Mind Like a Peace & Purposes</p>
+  </div>
+</div>
+</div>
+</div>
+
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <div class="border py-5 px-4"> 
+      <img src="./images/2560.jpg"  style="width: 100%;" height="200rem" alt="">
+
+      <p>Think Like a Monk: Train Your Mind Like a Peace & Purposes</p>
+
+  </div>
+      
+    </div>
+    <div class="carousel-item">
+    <div class="border py-5 px-4"> 
+      <img src="./images/2560.jpg"  style="width: 100%;" height="200rem" alt="">
+
+      <p>Think Like a Monk: Train Your Mind Like a Peace & Purposes</p>
+
+  </div>
+
+  <div class="carousel-item">
+    <div class="border py-5 px-4"> 
+      <img src="./images/2560.jpg"  style="width: 100%;" height="200rem" alt="">
+
+      <p>Think Like a Monk: Train Your Mind Like a Peace & Purposes</p>
+
+  </div>
+      
+    </div>
+    <div class="carousel-item">
+    <div class="border py-5 px-4"> 
+      <img src="./images/2560.jpg"  style="width: 100%;" height="200rem" alt="">
+
+      <p>Think Like a Monk: Train Your Mind Like a Peace & Purposes</p>
+
+  </div>
+     
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
