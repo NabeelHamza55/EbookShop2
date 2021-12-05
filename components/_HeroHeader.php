faf1457c11e0b8be27609e87716fa8c51ac115c4
HammaadKhan <nogain447khan@gmail.com>
2021-12-05 22:17:29 +0500

    Commit

--- components/_HeroHeader.php
+++ components/_HeroHeader.php
@@ -6,53 +6,53 @@
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-        <img style="height: 40rem;" src="./images/backgroud1.jpg" class="d-block w-100" alt="">
+        <img style="height: 40rem;" src="images/backgroud5.jpg" class="d-block w-100" alt="">
             <div class="carousel-caption d-none d-md-block">
                 <div class="row p-3 justify-content-between align-items-center">
-                    <div class="col-6 p-5">
-                        <p class="lead">THEBOOKWORM EDITORS'</p>
+                    <div class="col-6 ">
+                         <h3 class="head-color">THEBOOKWORM EDITORS</h3>
                         <h1 class="sty_le">
-                            Featured Book of the <br> <b> November</b>
+                            “Books are a uniquely portable magic.”
                         </h1>
                          <button type=""  class="get-ap btn btn-lg  ">See More</button>
                     </div>
                     <div class="col-6">
-                        <img style="width: 100%;" height="250" src="./images/2560.jpg" alt="">
+                        <img style="width: 100%;" height="250" src="images/caro 5.jpeg" alt="">
                     </div>
                 </div>
             </div>
         </div>
         <div class="carousel-item">
-            <img style="height: 40rem;" src="./images/backgroud1.jpg" class="d-block w-100" alt="">
+            <img style="height: 40rem;" src="images/backgroud5.jpg" class="d-block w-100" alt="">
             <div class="carousel-caption d-none d-md-block">
                 <div class="row p-3 justify-content-between align-items-center">
-                    <div class="col-6 p-5">
-                        <p class="lead">THEBOOKWORM EDITORS'</p>
+                    <div class="col-6 ">
+                        <h3 class="head-color">"Love is or it ain't"</h3>
                         <h1 class="sty_le">
-                            Featured Book of the <br> <b> November</b>
+                            “Take a good book to bed with you—books do not snore.”
                         </h1>
                         <button type=""  class="get-ap btn btn-lg  ">See More</button>
                     </div>
                     <div class="col-6">
-                        <img style="width: 100%;" height="250" src="./images/background.jpg" alt="">
+                        <img style="width: 100%;" height="250" src="images/caro 2.jpg" alt="">
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="carousel-item">
-            <img style="height: 40rem;" src="./images/backgroud1.jpg" class="d-block w-100" alt="">
+            <img style="height: 40rem;" src="images/backgroud5.jpg" class="d-block w-100" alt="">
             <div class="carousel-caption d-none d-md-block">
                 <div class="row p-3 justify-content-between align-items-center">
-                    <div class="col-6 p-5">
-                        <p class="lead">THEBOOKWORM EDITORS'</p>
+                    <div class="col-6 ">
+                        <h3 class="head-color">"In the end, we'll all become stories"</h3>
                         <h1 class="sty_le">
-                            Featured Book of The <br> <b> November</b>
+                            “A room without books is like a body without a soul.”   
                         </h1>
                         <button type=""  class="get-ap btn btn-lg  ">See More</button>
                     </div>
                     <div class="col-6">
-                        <img style="width: 100%;" height="250" src="./images/hassan.jpg" alt="">
+                        <img style="width: 100%;" height="250" src="../images/caro 1.jpg" alt="">
                     </div>
                 </div>
             </div>
